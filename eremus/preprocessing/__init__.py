b83f712c7f40d2b05211d47e029d9df0bebb5555
Antonio Pio Caruso <86917486+Luffy65@users.noreply.github.com>
2024-06-16 14:50:21 +0200

    updated __init__.py
    it was using a deprecated method

--- eremus/preprocessing/__init__.py
+++ eremus/preprocessing/__init__.py
@@ -1,9 +1,23 @@
 import pkgutil
+import importlib
+import importlib.util
+
+
+
+
+
+
 
 __all__ = []
-for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     if is_pkg or "." in module_name:
         continue
+    print(f"Importing module: {module_name}")  # Debug print
     __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+    try:
+        spec = importlib.util.find_spec(f'.{module_name}', package=__name__)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        globals()[module_name] = module
+    except Exception as e:
+        print(f"Error importing module {module_name}: {e}")  # Debug print
